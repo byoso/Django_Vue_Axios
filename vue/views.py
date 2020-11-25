ec4da5621c07569c462e73b25ca3c6a19fe4124e
byoso <emailvinz@gmail.com>
2020-11-25 14:59:56 +0100

    Minor fix

--- vue/views.py
+++ vue/views.py
@@ -7,14 +7,17 @@ def jsonify(a_query_set):
     """Convert a queryset into a json format"""
     attrs = []
     list_json = []
-    for i in a_query_set[0]._meta.fields:
-        attrs.append(i.name)
-    for i in a_query_set:
-        json_object = {}
-        for attr in attrs:
-            json_object[attr] = str(getattr(i, attr))
-        list_json.append(json_object)
-    return list_json
+    if len(a_query_set) > 0:
+        for i in a_query_set[0]._meta.fields:
+            attrs.append(i.name)
+        for i in a_query_set:
+            json_object = {}
+            for attr in attrs:
+                json_object[attr] = str(getattr(i, attr))
+            list_json.append(json_object)
+        return list_json
+    else:
+        return []
 
 
 def index(request):
